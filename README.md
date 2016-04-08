bf3b9817efb54e9064a5374e3c41fa335ce08854
Gonçalo Carito <goncalocarito@gmail.com>
2016-04-08 15:28:45 +0100

    add firefox 44- support, innerText as first option

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-** Simple Article Retrieval **
+# Simple Article Retrieval
 
-Retrieve articles from websites.
+## A website articles retrieval in Javascript
+
+It includes a *highlight* function for testing, allowing to see visually the
+words that are being selected.
+
+**innerText vs textContent**
+
+Both extract text from html content but have significant differences.
+[Kellegous.com](https://kellegous.com/j/2013/02/27/innertext-vs-textcontent/)
+
+`innerText` is compatible with all common browsers except Firefox 44-
+- A rule was added in this case to use `innerHTML` with regex replace.
+(showed better results than textContent - extensive tests can be down)
+
+`innerHTML` just have some minor problems with IE tables but since all versions
+of IE are going to use innerText by default, there is no problem.
--- article.js
+++ article.js
@@ -1,7 +1,6 @@
 var article = (function () {
 
-// highlights text
-// if you just want to retrieve comment the invocation
+// to highlight text in the browser
 function doSearch(text) {
     // from http://stackoverflow.com/a/5887719/2237234
     if (window.find && window.getSelection) {
@@ -23,9 +22,19 @@ function doSearch(text) {
     }
 }
 
+// HTML DOM extraction
+function DOMExtract(domobj){
+  if ('innerText' in domobj) {
+      return sent = domobj.innerText.split("\n").filter(function(n){ if(n.indexOf('.') !== -1 || n.indexOf('?') !== -1 || n.indexOf('!') !== -1 ) return n });
+  } else {
+      return sent = domobj.innerHTML.replace(/<\/?[^>]+>/gi,"").split("\n").filter(function(n){ if(n.indexOf('.') !== -1 || n.indexOf('?') !== -1 || n.indexOf('!') !== -1 ) return n });;
+  }
+}
 
-var sent = document.body.textContent.split("\n").filter(function(n){return n.match(/^[^<>\{\}_]+$/)}).filter(function(n){ if(n.indexOf('.') !== -1 || n.indexOf('?') !== -1 || n.indexOf('!') !== -1 ) return n });;
+sent = DOMExtract(document.body);
+console.log(sent);
 
+// just for testing
 for (var i in sent) {
     console.log(sent[i]);
     doSearch(sent[i]);
